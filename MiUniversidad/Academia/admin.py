585656cb5974e6b989f13366c510b75c03e92f34
santiago <sochoa16.finca@gmail.com>
2021-10-21 10:46:18 -0500

    Taller02 añadido

---
+++ MiUniversidad/Academia/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from Academia.models import Carrera, Curso, Estudiante, Matricula
+
+# Register your models here.
+
+admin.site.register(Carrera)
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+admin.site.register(Matricula)
